b2c09ea9d161f1509e1de84a033ceda000d31100
sanjith1611 <sanjith1611@gmail.com>
2024-10-21 12:09:01 +0530

    fi

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,19 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React,{useState} from "react";
 import "./App.css";
-import NameChange from "./Component/NameChange";
-import CounterComp from "./component/CounterComp";
+// import NameChange from "./Component/NameChange";
+// import CounterComp from "./component/CounterComp";
 
-import PorpsEg from "./Component/PropsEg";
-import ApiCallWithBtn from "./Component/ApiCallWithBtn";
-import LifeCycleFunComp from "./Component/LifeCycleFunComp";
-import FormReact from "./Component/FormReact";
-import ReactFormHook from "./Component/ReactFormHook";
+// import PorpsEg from "./Component/PropsEg";
+// import ApiCallWithBtn from "./Component/ApiCallWithBtn";
+// import LifeCycleFunComp from "./Component/LifeCycleFunComp";
+// import FormReact from "./Component/FormReact";
+// import ReactFormHook from "./Component/ReactFormHook";
 
-import ProductComp from "./Component/ProductComp";
-import TodoList from "./Component/TodoList";
-import DummyPractice from './Component/DummyPractice';
-import TodoWrapper from './Component/TodoWrapper';
+// import ProductComp from "./Component/ProductComp";
+// import TodoList from "./Component/TodoList";
+// import DummyPractice from './Component/DummyPractice';
+// import TodoWrapper from './Component/TodoWrapper';
 
 
 function App(){
