a6b718304b6324ac356de6d7c92565011c509ade
sanjith1611 <sanjith1611@gmail.com>
2024-10-26 10:43:19 +0530

    source

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,24 @@
 
-import 'bootstrap/dist/css/bootstrap.min.css';
 import React,{useState} from "react";
-import "./App.css";
-// import NameChange from "./Component/NameChange";
-// import CounterComp from "./component/CounterComp";
-
-// import PorpsEg from "./Component/PropsEg";
-// import ApiCallWithBtn from "./Component/ApiCallWithBtn";
-// import LifeCycleFunComp from "./Component/LifeCycleFunComp";
-// import FormReact from "./Component/FormReact";
-// import ReactFormHook from "./Component/ReactFormHook";
-
-// import ProductComp from "./Component/ProductComp";
-// import TodoList from "./Component/TodoList";
-// import DummyPractice from './Component/DummyPractice';
-// import TodoWrapper from './Component/TodoWrapper';
-
-
-function App(){
-// // const[name,setname]=useState("sanjith")
-// //  const[age,setage]=useState(22) 
-// const[islogin,setislogin] = useState(true)
-
-// const handlelogout=()=>{
-// setislogin(false)
-
-// }
-
+import"./App.css";
+import PropsExample from "./Component/PropsExample"
+import ApiCallWithBtn from "./Component/ApiCallWithBtn";
+import LifeCycleFunComp from "./Component/LifeCycleFunComp";
+import FormReact from "./Component/FormReact";
+import  PracticeReact from"./Component/PracticeReact"
+import ReactHook from "./Component/ReactHook"
+function App() {
+// const[name,setname]=useState("sanjith")
+// const[age,setage]=useState(21)
   return(
 <>
-
-{/* <FormReact/> */}
-{/* <ReactFormHook/> */}
-{/* <ProductComp/> */}
-{/* <TodoList/> */}
-{/* <ReactFormHook/> */}
-
-{/* <DummyPractice/>  */}
+{/* <PropsExample a={name} b={age}/> */}
 {/* <ApiCallWithBtn/> */}
-{/* 
-<TodoWrapper/> */}
+{/* <LifeCycleFunComp/> */}
+{/* <FormReact/> */}
+{/* <PracticeReact/> */}
+<ReactHook/>
 
-
-
-<h2>new one  rerwerew</h2>
 </>
 
 
